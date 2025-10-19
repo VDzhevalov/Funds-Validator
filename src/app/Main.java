56f05efa36ce4ad335f44377621e6c206d1aed94
Volodymyr Dzhevalov <vladtestac@gmail.com>
2025-10-19 17:43:09 +0300

    Add the validateAmount() method to check whether a withdrawal is possible.

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,6 +6,21 @@ public class Main {
 
     }
 
+    // Метод валідації наявних коштів
+    private static void validateAmount(double balance, double withdrawal) {
+        if (withdrawal > balance) {
+            try {
+                throw new FundsException("Insufficient funds!");
+            } catch (FundsException ex) {
+                System.out.println(ex.getMessage());
+            }
+        } else {
+            balance = getBalance(balance, withdrawal);
+            System.out.printf("Funds are OK. Purchase paid." +
+                    "%nBalance is USD %.2f", balance);
+        }
+    }
+
     // Метод розрахунку наявних коштів на рахунку
     // після зняття певної суми коштів
     private static double getBalance(double balance, double withdrawal) {
