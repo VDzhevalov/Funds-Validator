bb75a7c44ad9043855171f98d36cb7f9f2b8be1a
Volodymyr Dzhevalov <vladtestac@gmail.com>
2025-10-20 01:08:31 +0300

    Change Main class for new application implementation.

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,56 +1,10 @@
 package app;
 
-import app.exceptions.FundsException;
-
-import java.util.Scanner;
-
-import static app.validator.InputValidator.validateInputAmount;
+import app.utils.AppStarter;
 
 public class Main {
-    static double balance;
 
     public static void main(String[] args) {
-        balance = getBalance();
-        validateAmount(balance, getAmount());
-    }
-
-    private static double getBalance() {
-        return 1000.00; // Наявні кошти на рахунку
-    }
-
-    private static double getAmount() {
-        System.out.printf("Balance is USD %.2f.%n" +
-                "Enter purchase amount, USD: ", balance);
-        String amount;
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            amount = scanner.nextLine();
-            if (!validateInputAmount(amount)) {
-                continue;
-            }
-            break;
-        }
-        return Double.parseDouble(amount);
-    }
-
-    // Метод валідації наявних коштів
-    private static void validateAmount(double balance, double withdrawal) {
-        if (withdrawal > balance) {
-            try {
-                throw new FundsException("Insufficient funds!");
-            } catch (FundsException ex) {
-                System.out.println(ex.getMessage());
-            }
-        } else {
-            balance = getBalance(balance, withdrawal);
-            System.out.printf("Funds are OK. Purchase paid." +
-                    "%nBalance is USD %.2f", balance);
-        }
-    }
-
-    // Метод розрахунку наявних коштів на рахунку
-    // після зняття певної суми коштів
-    private static double getBalance(double balance, double withdrawal) {
-        return balance - withdrawal;
+        AppStarter.runApp();
     }
 }
