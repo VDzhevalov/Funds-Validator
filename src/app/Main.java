ab04dad80159c18964986f6cd3e4e49a8c682533
Volodymyr Dzhevalov <vladtestac@gmail.com>
2025-10-19 18:20:11 +0300

    Move FundsException to exceptions package.

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,7 @@
 package app;
 
+import app.exceptions.FundsException;
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
--- src/app/exceptions/FundsException.java
+++ src/app/exceptions/FundsException.java
@@ -1,4 +1,4 @@
-package app;
+package app.exceptions;
 
 public class FundsException extends RuntimeException {
 
