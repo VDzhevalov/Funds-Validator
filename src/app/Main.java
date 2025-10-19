d3f44705694dd382aea89aecb86d81971650167c
Volodymyr Dzhevalov <vladtestac@gmail.com>
2025-10-19 17:39:40 +0300

    Add the getBalance() method to obtain the amount of money after withdrawal.

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,14 @@
 package app;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    // Метод розрахунку наявних коштів на рахунку
+    // після зняття певної суми коштів
+    private static double getBalance(double balance, double withdrawal) {
+        return balance - withdrawal;
+    }
 }
