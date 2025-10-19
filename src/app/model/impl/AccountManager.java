b29e839c42346c6d723ad4603835318ae395d1ee
Volodymyr Dzhevalov <vladtestac@gmail.com>
2025-10-20 00:56:23 +0300

    Add AccountManager to manage operations with accounts.

---
+++ src/app/model/impl/AccountManager.java
@@ -0,0 +1,24 @@
+package app.model.impl;
+
+import app.entity.Account;
+import app.enums.Currency;
+import app.exceptions.FundsException;
+
+import static app.validator.OperationValidator.*;
+
+public class AccountManager {
+
+    public static double withdrawMoney(Account account, double amount, Currency currency) throws FundsException {
+        if (isValidCurrency(account, currency) && isValidAmount(account, amount)) {
+            account.changeBalance(account.getBalance() - amount);
+        }
+        return account.getBalance();
+    }
+
+    public static double depositMoney(Account account, double amount, Currency currency) throws FundsException {
+        if (isValidCurrency(account, currency)){
+            account.changeBalance(account.getBalance() + amount);
+        }
+        return account.getBalance();
+    }
+}
