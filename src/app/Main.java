7d052a07861efb626650fa13f3208b31d728aeb7
Volodymyr Dzhevalov <vladtestac@gmail.com>
2025-10-19 17:53:25 +0300

    Add the getBalance(), getAmount() methods and main program flow.

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,9 +1,24 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
+    static double balance;
 
     public static void main(String[] args) {
+        balance = getBalance();
+        validateAmount(balance, getAmount());
+    }
 
+    private static double getBalance() {
+        return 1000.00; // Наявні кошти на рахунку
+    }
+
+    private static double getAmount() {
+        System.out.printf("Balance is USD %.2f.%n" +
+                "Enter purchase amount, USD: ", balance);
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextDouble();
     }
 
     // Метод валідації наявних коштів
